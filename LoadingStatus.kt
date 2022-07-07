a8dcbed70936bbe319d18ab6ef97c0a38a717cc1
Mukesh <43924254+mukeshvdavara@users.noreply.github.com>
2022-07-07 22:32:33 +0530

    Create LoadingStatus.kt

---
+++ LoadingStatus.kt
@@ -0,0 +1,7 @@
+
+
+sealed class LoadingStatus<out T> {
+    object Loading : LoadingStatus<Nothing>()
+    data class Success<T>(val data: T) : LoadingStatus<T>()
+    data class Error(val throwable: Throwable) : LoadingStatus<Nothing>()
+}
